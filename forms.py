3359c415881945296e5ad15c542aa6c085e8f482
Daniel Šup <supdanie@fit.cvut.cz>
2020-02-04 14:46:18 +0100

    first commit

---
+++ forms.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.core.validators import FileExtensionValidator
+
+from django import forms
+
+class MyForm(forms.Form):
+    field = forms.FileField(label='Upload video', validators=[FileExtensionValidator(allowed_extensions=["mp4", "avi", "flv"])])
+    action = forms.ChoiceField(choices=[('watch', 'Watch the video with speeds of the vehicles'),
+                                         ('download', 'Download video with speeds of the vehicles')], widget=forms.RadioSelect, label='')
---
+++ pages/views.py
@@ -0,0 +1,32 @@
+import os
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from forms import MyForm
+from speedcheck import loadInputAndProcess
+from django.conf import settings
+
+
+import datetime
+
+def home_view(request, *args, **kwargs):
+    today = datetime.datetime.now().date()
+
+    if request.method=='POST':
+        form = MyForm(request.POST, request.FILES)
+        if form.is_valid():
+            action = request.POST['action']
+            title = str(request.FILES['field'])
+            content = request.FILES['field'].read()
+            f = open(str(settings.MEDIA_ROOT)+title, "wb")
+            f.write(content)
+            f.close()
+            loadInputAndProcess('media/'+title, action == 'watch')
+            if action == 'watch':
+
+                return render(request, "myaction.html", {'value': title})
+            else:
+                return render(request, "showlink.html", {'value': "output.avi"})
+
+    form = MyForm()
+    return render(request, "home.html", {"today" : today, 'form': form })
